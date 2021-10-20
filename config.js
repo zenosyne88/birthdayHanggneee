30320d250d5af907389ea4f90153b45b83d2bb92
zenosyne88 <92872151+zenosyne88@users.noreply.github.com>
2021-10-20 22:28:13 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
+    titleWeb: "Tuổi 19 của Thế Long",
+    introTitle: 'Nguyến Thế Long ',
+    introDesc: `Chúc sinh nhật muộn màng bạn nhé
+    Lòng chân thành nhỏ bé gửi trao
+    Chúc bạn sức khoẻ dồi dào
+    Gia đình hạnh phúc ngọt ngào yêu thương`,
+    btnIntro: '^^hpbd^^',
+    title: 'chúc mừng sinh nhật ml trẻ trâu  🥰',
+    desc: 'đôi lúc t cũng ghét m lắm đó nhưng mà ',
+    btnYes: 't không ghét m',
+    btnNo: 't ghét hbn vcl :3',
+    question: 'học chung với nhau từ cấp 2 lên đến cấp 3 coi như là có duyên ít nhiều',
+    btnReply: 'bấm đây để xác nhận bạn sẽ luôn vui vẻ ',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    messDesc: 'Tết về lại mời mình đi ăn nhé.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
