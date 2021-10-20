b2d94d4ec72f73b0831a40a2439f1d466f5cd1b0
zenosyne88 <92872151+zenosyne88@users.noreply.github.com>
2021-10-20 22:32:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     titleWeb: "Tuổi 19 của Thế Long",
-    introTitle: 'Nguyễn Thế Long ',
+    introTitle: 'Thế Long ',
     introDesc: `Chúc sinh nhật muộn màng bạn nhé
     Lòng chân thành nhỏ bé gửi trao
     Chúc bạn sức khoẻ dồi dào
